6639d16063d795a5ef33c748e79508bb8dd1b150
iinoten <nteten0313@icloud.com>
2023-07-14 02:59:10 +0900

    Add 横線を追加、値に合わせて縦の入力を変更するようにした

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { Circle, Layer, Line, Rect, Stage } from "react-konva";
 import './styles.css';
 
 // 開発用に使うデモの値
-const VALUE_DEMO = [[0,0],[0.3,0.6],[0.5,0.3],[0.55,0.7],[1,0.5]]
+const VALUE_DEMO = [[0,0],[0.3,0.6],[0.5,0.3],[0.55,0.7],[1,1]]
 
 const SimpleChart = () => {
     let biggistValIndex = 0;
@@ -16,9 +16,9 @@ const SimpleChart = () => {
     let renderPointArr = []
 
     for (let index = 0; index < VALUE_DEMO.length; index++) {
-        renderLineArr[index*2] = VALUE_DEMO[index][0]*780+10
-        renderLineArr[index*2+1] = 390 - VALUE_DEMO[index][1]*390 + 10
-        renderPointArr.push([VALUE_DEMO[index][0]*780+10,390-VALUE_DEMO[index][1]*390 + 10])
+        renderLineArr[index*2] = VALUE_DEMO[index][0]*710+80
+        renderLineArr[index*2+1] = 390 - VALUE_DEMO[index][1] / VALUE_DEMO[biggistValIndex][1] *390 + 10
+        renderPointArr.push([VALUE_DEMO[index][0]*710+10+70,390-VALUE_DEMO[index][1]*390 + 10])
     }
 
     return(
@@ -26,7 +26,14 @@ const SimpleChart = () => {
             <Stage width={800} height={450}>
                 <Layer>
                   <Rect stroke='black' strokeWidth={0.1} width={800} height={450} />
-                  <Line points={[10,10, 10,400, 790,400]}stroke='#696969' strokeWidth={3} />
+                    {(() => {
+                        const memoryLines = []
+                        for (let index = 0; index < 10; index++) {
+                            memoryLines.push(<Line points={[80, 400-40*index, 790,400-40*index,]} radius={6} stroke='#696969' strokeWidth={1} />)
+                        }
+                        return memoryLines
+                    })()}
+                    
                   <Line points={renderLineArr}stroke='#696969' strokeWidth={3} />
                     {renderPointArr.map((item) => {
                         return (
