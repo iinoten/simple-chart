45c519db7043532fc56d3589e1354b0e88744770
iinoten <nteten0313@icloud.com>
2023-07-14 00:02:18 +0900

    Add グラフの最初の枠を追加

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import { Layer, Line, Rect, Stage } from "react-konva";
 import './styles.css';
-const MyComponent = () => (
-    <h1>Hello from My Component</h1>
+
+const SimpleChart = () => (
+    <>
+        <Stage width={800} height={450}>
+            <Layer>
+              <Rect stroke='black' strokeWidth={0.1} width={800} height={450} />
+              <Line points={[10,10, 10,400, 790,400]}stroke='#696969' strokeWidth={3} />
+            </Layer>
+        </Stage>
+    </>
 );
-export default MyComponent;
+export default SimpleChart;
