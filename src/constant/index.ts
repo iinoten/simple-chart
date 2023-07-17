954af95b4fbcc86f540eb3c47520c7b6d76cfea0
iinoten <nteten0313@icloud.com>
2023-07-17 21:45:22 +0900

    Add カラーコードの定数を追加

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -5,7 +5,19 @@ export interface DataEntry {
 
 export interface SimpleChartLineChildProps {
     dataEntry: DataEntry[];
-    color: ColorCode
+    color: ColorCode;
+    title: string;
 }
 
 export type ColorCode = `#${string}`
+
+export const preColors:ColorCode[] = [
+    '#ff4500',
+    '#3cb371',
+    '#1e90ff',
+    '#6a5acd',
+    '#ff1493',
+    '#ff8c00',
+    '#b22222',
+    '#008b8b',
+]
